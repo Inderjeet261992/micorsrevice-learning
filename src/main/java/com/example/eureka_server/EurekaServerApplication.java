d495cb6b5b91d4d86a60218887b1aaa5cea7cf34
Inderjeet261992 <inderjeetkaurhorahora@gmail.com>
2025-06-09 21:35:40 +0530

    Update EurekaServerApplication.java

--- src/main/java/com/example/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/example/eureka_server/EurekaServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder
 
 @SpringBootApplication
 @EnableEurekaServer
